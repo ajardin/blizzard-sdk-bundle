9b4e03f5fd3f1da1b6cf182fa03ad1a9463b5ab8
Alexandre Jardin <info@ajardin.fr>
2022-10-10 19:54:44 +0200

    Use XML configuration files instead of PHP

--- src/DependencyInjection/AjardinBlizzardSdkExtension.php
+++ src/DependencyInjection/AjardinBlizzardSdkExtension.php
@@ -7,7 +7,7 @@ namespace Ajardin\BlizzardSdkBundle\DependencyInjection;
 use Ajardin\BlizzardSdkBundle\BlizzardApi\Authentication\Credentials;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Loader\PhpFileLoader;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 final class AjardinBlizzardSdkExtension extends Extension
@@ -17,14 +17,14 @@ final class AjardinBlizzardSdkExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container): void
     {
-        $loader = new PhpFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.php');
-        $loader->load('services/world_of_warcraft.php');
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.xml');
+        $loader->load('services/world_of_warcraft.xml');
 
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
-        $definition = $container->getDefinition(Credentials::class);
+        $definition = $container->getDefinition('ajardin_blizzard_sdk.credentials');
         $definition->replaceArgument('$clientId', $config['blizzard']['client_id']);
         $definition->replaceArgument('$clientSecret', $config['blizzard']['client_secret']);
     }
